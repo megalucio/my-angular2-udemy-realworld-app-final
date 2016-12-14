3e92dd7517a164057026b1b107a28c0b3613b416
megalucio <megalucio@gmail.com>
2016-12-14 15:22:36 -0500

    136. Adding a Navbar

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,20 @@
 import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
-  template: '<h1>My First Angular App</h1>'
+  template: `
+      <nav class="navbar navbar-default">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <a class="navbar-brand" href="#">ngProject</a>
+          </div>
+          <div class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+              <li><a href="#">Users</a></li>
+              <li><a href="#">Posts</a></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+  `
 })
 export class AppComponent { }
